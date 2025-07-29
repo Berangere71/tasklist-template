ba39fa41b1706245aae64d9067310e910f942f91
Bérangère71 <berangerebrunat@hotmail.fr>
2025-07-29 14:56:47 +0200

    https://github.com/Berangere71/tasklist-template.git

--- inscription.php
+++ inscription.php
@@ -1,20 +1,52 @@
+
+
 <?php
 require_once "bdd-crud.php";
 
-?>
+$host = "127.0.0.1";
+$database = "app-database";
+$bdd = new PDO("mysql:host=$host;dbname=$database", "root", "root");
 
+// Vérifiez si le formulaire a été soumis
+if (isset($_POST["email"]) && isset($_POST["password"])) {
+    // Préparez la requête d'insertion
+    $request = $bdd->prepare("INSERT INTO User(email, password) VALUES(?, ?)");
+    
+    // Exécutez la requête avec les valeurs du formulaire
+    $request->execute([$_POST["email"], $_POST["password"]]);
+    
+    // Affichez le résultat de l'exécution
+    $resultat = $request;
+    var_dump($resultat);
+}
+?>
 
 <!DOCTYPE html>
 <html lang="fr">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inscription</title>
 </head>
-
 <body>
-   <!-- TODO Formulaire pour s'inscrire (créer un utilisateur) -->
-</body>
 
+<h1>INSCRIPTION</h1>
+
+<div class="User">
+    <?php foreach($User as $users): ?>
+        <div class="users"> 
+            <h2><?= 'email: ' . htmlspecialchars($users['email']); ?></h2>
+            <h2><?= 'password: ' . htmlspecialchars($users['password']); ?></h2>
+        </div>
+    <?php endforeach; ?>
+</div>
+
+<form action="User" method="post">
+    <input type="email" name="email" placeholder="Entrez votre email" required>
+    <input type="password" name="password" placeholder="Entrez votre mot de passe" required>
+    <button type="submit">s'inscrire</button>
+</form>
+
+</body>
 </html>
+
--- login.php
+++ login.php
@@ -2,8 +2,29 @@
 require_once "bdd-crud.php";
 // TODO Connection Utilisateur via la session
 
-?>
+$host = "127.0.0.1";
+$database = "app-database";
+$bdd = new PDO("mysql:host=$host;dbname=$database", "root", "root");
 
+// Vérifiez si le formulaire a été soumis
+if (isset($_POST["email"]) && isset($_POST["password"])) {
+    // Préparez la requête de sélection
+    $request = $bdd->prepare("SELECT * FROM User WHERE email = ?");
+    
+    // Exécutez la requête avec l'email fourni
+    $request->execute([$_POST["email"]]);
+    $User = $request->fetch(PDO::FETCH_ASSOC);
+
+    // Vérifiez si l'utilisateur existe et si le mot de passe correspond
+    if ($User && $_POST["password"] == $User["password"]) {
+        // Affichez le résultat de l'exécution
+        $resultat = $request;
+        var_dump($resultat);
+    } else {
+        echo "Identifiants incorrects.";
+    }
+}
+?>
 
 <!DOCTYPE html>
 <html lang="fr">
@@ -15,7 +36,6 @@ require_once "bdd-crud.php";
 <body>
     <h1>Connexion</h1>
     <!-- TODO Formulaire de connexion -->
-
     <a href="inscription.php">Pas de compte ? S'inscrire</a>
 </body>
 </html>
